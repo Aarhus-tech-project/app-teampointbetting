cf0f4be967a5e3c111365c7b31a97ff1233ea1fa
John Risager <risagerjohnb@gmail.com>
2025-10-28 11:18:01 +0100

    User can answer bets

--- backend/DotNet8Authentication-main/DotNet8Authentication/Controllers/BetAnswerController.cs
+++ backend/DotNet8Authentication-main/DotNet8Authentication/Controllers/BetAnswerController.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet8Authentication.Data;
+using DotNet8Authentication.DTO;
 using DotNet8Authentication.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -20,23 +21,25 @@ namespace DotNet8Authentication.Controllers
         }
 
         [HttpPost, Authorize]
-        public async Task<IActionResult> SubmitAnswer([FromBody] BetAnswer betAnswer)
+        public async Task<IActionResult> SubmitAnswer([FromBody] CreateBetAnswerDto dto)
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
                 return Unauthorized();
             }
-            // Example logic: Deduct points for placing a bet answer
-            user.Points = 500;
-            const int costToPlaceBetAnswer = 50;
-            if (user.Points < costToPlaceBetAnswer)
+
+            var betAnswer = new BetAnswer
             {
-                return BadRequest("Insufficient points to place a bet answer.");
-            }
-            user.Points -= costToPlaceBetAnswer;
+                BetAnswerId = Guid.NewGuid(),
+                BetId = dto.BetId,
+                UserId = Guid.Parse(user.Id),
+                Answer = dto.Answer.ToLower(),
+                BettedPoints = dto.BettedPoints,
+                SubmittedAt = DateTime.UtcNow
+            };
+
             _context.BetAnswers.Add(betAnswer);
-            await _userManager.UpdateAsync(user);
             await _context.SaveChangesAsync();
             return Ok(betAnswer);
         }
---
+++ backend/DotNet8Authentication-main/DotNet8Authentication/DTO/CreateBetAnswerDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DotNet8Authentication.DTO
+{
+    public class CreateBetAnswerDto
+    {
+        public Guid BetId { get; set; }
+        public string Answer { get; set; }
+        public int BettedPoints { get; set; }
+    }
+}
